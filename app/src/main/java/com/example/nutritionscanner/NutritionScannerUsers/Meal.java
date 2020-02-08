55834f18820724d36cd3e657df3b052abae5537c
Menarul Alam <menarulalam@gmail.com>
2020-02-08 15:40:22 -0500

    Added meal

--- app/src/main/java/com/example/nutritionscanner/NutritionScannerUsers/Meal.java
+++ app/src/main/java/com/example/nutritionscanner/NutritionScannerUsers/Meal.java
@@ -1,4 +1,27 @@
 package com.example.nutritionscanner.NutritionScannerUsers;
 
 public class Meal {
-}
+    enum Type {
+        BREAKFAST, LUNCH, DINNER, SNACK
+    }
+    Type type;
+    private int[] numCalories;
+    public Meal(String mealTime, int calFat, int calCarb, int calProtein){
+        int[] numCalories = new int[]{calFat, calCarb, calProtein};
+        mealTime=mealTime.toUpperCase();
+        switch(mealTime){
+            case "BREAKFAST":
+                type=Type.BREAKFAST;
+                break;
+            case "LUNCH":
+                type = Type.LUNCH;
+                break;
+            case "DINNER":
+                type = Type.DINNER;
+                break;
+            default:
+                type = Type.SNACK;
+        }
+    }
+    }
+
